91b37ae9cd73b3babc32743724b44226adb04f1c
Allison Valdez <ajsnipes@usfca.edu>
2022-01-06 12:39:32 -0800

    working on creating a function for creating an alarm

---
+++ Mod1/main.py
@@ -0,0 +1,24 @@
+"""
+Created by Allison Valdez
+January 6, 2021
+Project: Alarm Clock
+
+See the README for important project notes and intentions for the project.
+"""
+
+"""
+Tkinter: creates a dialog box to provide: information or to method to gather 
+it from the user, a fast & easy way to create GUI applications, 
+and a powerful object orientated interface to the TK GUI toolkit.
+datetime: combines the date and time information gathered from the module (
+mostly for dates)
+time: usually used for Unix systems timestamps (mostly for timing)
+winsound: enables sound playing mechanism in window systems
+"""
+from tkinter import *
+import datetime
+import time
+import winsound
+
+
+
