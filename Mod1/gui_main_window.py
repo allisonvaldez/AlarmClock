2a522dacff3d563527eee4ad317e93af225f17fb
Allison Valdez <ajsnipes@usfca.edu>
2022-01-18 17:46:35 -0800

    redoing complete overhaul

--- Mod1/gui_main_window.py
+++ Mod1/gui_main_window.py
@@ -38,7 +38,10 @@ class MainWindow:
         # sets the background color of the window
         self.root.config(background="#FFFFFF")
 
-    def Threading(self):
+        # runs the event loop to monitor events/changes in the GUI window
+        self.root.mainloop()
+
+    def threading(self):
         """
         This function is in charge of being able to run multiple threads (
         tasks and/or function calls) at the same time. It allows python to
@@ -66,6 +69,8 @@ class MainWindow:
         while True:
             print(f"while true statement triggered")
 
+            set_alarm = f"{hour.get()}:{minute.get()}:{second.get()}"
+
             """
             .sleep(1) halts the execution of further commands by 1 second until we 
             receive the time or prompt from the user.
@@ -74,19 +79,11 @@ class MainWindow:
             time.sleep(1)
 
             """
-            get the current time (date and time) and save it to the current_time 
-            variable to use later
+            Get the current time (date and time) and use string conversion to
+            format it to display as H:M:S.
             """
-            current_time = datetime.datetime.now()
-
-            # string conversion to format the time to display as H:M:S
-            current_time_now = current_time.strftime("%H:%M%S")
-
-            # string conversion to format the date to display as d/m/y
-            current_date_now = current_time.strftime("%d/%m/%Y")
-
-            print(f"The date set is:", current_date_now)
-            print(f"The current time is", current_time_now)
+            current_time = datetime.datetime.now().strftime("%H:%M%S")
+            print(f"current time: {current_time} alarm set: {set_alarm}")
 
             """
             Set an if statement to check if the timer is the same time as the 
@@ -94,88 +91,58 @@ class MainWindow:
             Tried to utilize winsound but it will not import for me to call the 
             functions to sound the alarm--but I chose to use beepy instead.
             """
-            if current_time_now == timer:
+            if current_time == set_alarm:
                 print(f"if current time = alarm if statement triggered")
-                print(f"The alarm sounded. It is:", current_time_now)
-                beep(sound="ping")
+                print(f"The alarm sounded. It is: {current_time}")
+                winsound.PlaySound("sound.wav", winsound.SND_ASYNC)
                 break
+    """
+    Here we can use the global level to declare the attributes and needed 
+    elements for the GUI window. 
+    """
 
-            """
-                    sets the hour variable to a string and makes it to automatically 
-                    populate with 00 when it is displayed.
-                    """
-            hour = StringVar(self.root)
-            hours = ('00', '01', '02', '03', '04', '05', '06', '07', '08', '09',
-                     '10', '11', '12', '13', '14', '15', '16', '17', '18', '19',
-                     '20', '21', '22', '23', '24')
-            hour.set(hours[0])
-            hrs = OptionMenu(frame, hour, *hours)
-            hrs.pack(side=LEFT)
+    """
+    Sets the hour variable to a string and makes it to automatically populate 
+    with 00 when it is displayed.
+    """
+    hour = StringVar(self.root)
+    hours = ('00', '01', '02', '03', '04', '05', '06', '07', '08', '09',
+             '10', '11', '12', '13', '14', '15', '16', '17', '18', '19',
+             '20', '21', '22', '23', '24')
+    hour.set(hours[0])
+    hrs = OptionMenu(frame, hour, *hours)
+    hrs.pack(side=LEFT)
 
-            minute = StringVar(self.root)
-            minutes = (
-                '00', '01', '02', '03', '04', '05', '06', '07', '08', '09',
-                '10', '11', '12', '13', '14', '15', '16', '17', '18', '19',
-                '20', '21', '22', '23', '24', '25', '26', '27', '28', '29',
-                '30', '31', '32', '33', '34', '35', '36', '37', '38', '39',
-                '40', '41', '42', '43', '44', '45', '46', '47', '48', '49',
-                '50', '51', '52', '53', '54', '55', '56', '57', '58', '59',
-                '60')
-            minute.set(minutes[0])
+    minute = StringVar(self.root)
+    minutes = (
+        '00', '01', '02', '03', '04', '05', '06', '07', '08', '09',
+        '10', '11', '12', '13', '14', '15', '16', '17', '18', '19',
+        '20', '21', '22', '23', '24', '25', '26', '27', '28', '29',
+        '30', '31', '32', '33', '34', '35', '36', '37', '38', '39',
+        '40', '41', '42', '43', '44', '45', '46', '47', '48', '49',
+        '50', '51', '52', '53', '54', '55', '56', '57', '58', '59',
+        '60')
+    minute.set(minutes[0])
 
-            mins = OptionMenu(frame, minute, *minutes)
-            mins.pack(side=LEFT)
+    mins = OptionMenu(frame, minute, *minutes)
+    mins.pack(side=LEFT)
 
-            second = StringVar(self.root)
-            seconds = (
-                '00', '01', '02', '03', '04', '05', '06', '07', '08', '09',
-                '10', '11', '12', '13', '14', '15', '16', '17', '18', '19',
-                '20', '21', '22', '23', '24', '25', '26', '27', '28', '29',
-                '30', '31', '32', '33', '34', '35', '36', '37', '38', '39',
-                '40', '41', '42', '43', '44', '45', '46', '47', '48', '49',
-                '50', '51', '52', '53', '54', '55', '56', '57', '58', '59',
-                '60')
-            second.set(seconds[0])
+    second = StringVar(self.root)
+    seconds = (
+        '00', '01', '02', '03', '04', '05', '06', '07', '08', '09',
+        '10', '11', '12', '13', '14', '15', '16', '17', '18', '19',
+        '20', '21', '22', '23', '24', '25', '26', '27', '28', '29',
+        '30', '31', '32', '33', '34', '35', '36', '37', '38', '39',
+        '40', '41', '42', '43', '44', '45', '46', '47', '48', '49',
+        '50', '51', '52', '53', '54', '55', '56', '57', '58', '59',
+        '60')
+    second.set(seconds[0])
 
-            secs = OptionMenu(frame, second, *seconds)
-            secs.pack(side=LEFT)
+    secs = OptionMenu(frame, second, *seconds)
+    secs.pack(side=LEFT)
 
-            Button(self.root, text="Set Alarm", font=("Helvetica 15 bold"),
-                   command=Threading).pack(pady=20)
+    Button(self.root, text="Set Alarm", font=("Helvetica 15 bold"),
+           command=Threading).pack(pady=20)
 
-    def start_clock(self):
-        """
-        This function is in charge of monitoring the GUI interfaces for any
-        changes and updates it as necessary.
 
-        :return: the proper GUI interface update
-        """
-        print(f"begin clock function triggered")
 
-        """
-        runs the event loop to monitor for events or changes for the GUI window
-        """
-        self.root.mainloop()
-
-    def actual_time(hours, mins, secs):
-        """
-        This function takes in the user input (in the proper string format) for the
-        alarm time they wish to trigger. This takes the same parameter argument
-        as the function above.
-
-        :param hours: parameter of hour gathered from the user
-        :param mins: parameter of min gathered from the user
-        :param secs: parameter of second gathered from the user
-        :return: the proper time format gathered from the user
-        """
-
-        print(f"actual time function triggered")
-        # Gets the time input from the user
-        timer = f"{hours.__get__()}:{mins.__get__()}:{secs.__get__()}"
-        hours.__get__()
-
-        """
-        triggers the alarm function above based on the input gathered from the
-        user
-        """
-        sound_alarm(timer)
